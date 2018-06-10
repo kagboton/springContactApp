44cbada5b888f2b715aaec92ce6b94014a07391a
Kilian Agboton <kilian.agboton@etu.univ-orleans.fr>
2018-06-10 02:12:39 +0200

    Working files initialization

---
+++ src/main/java/com/kagboton/scapp/SpringContactAppApplication.java
@@ -0,0 +1,12 @@
+package com.kagboton.scapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringContactAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringContactAppApplication.class, args);
+	}
+}
